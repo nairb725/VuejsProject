2e9a3a842ba4d27b72b4689806458929f4ab6139
Brian <nairb725@gmail.com>
2021-10-13 14:56:57 +0200

    glouglou logo search bar

--- my-app/src/views/Home.vue
+++ my-app/src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>
+      <span class="blue">G</span>
+      <span class="red">l</span>
+      <span class="yellow">o</span>
+      <span class="green">u</span>
+      <span class="blue">g</span>
+      <span class="red">l</span>
+      <span class="yellow">o</span>
+      <span class="green">u</span>
+    </h1>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -15,3 +24,19 @@ export default {
   }
 }
 </script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1{
+  font-size: 150px;
+  font-family:"Product sans", sans-serif
+}
+.blue{
+  color:#4285f4;
+}
+.red{
+  color:#ea4335;
+}
+
+.yellow{color:#fbbc05;}
+.green{color:#34a853}
+</style>
