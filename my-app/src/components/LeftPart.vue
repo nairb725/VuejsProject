7f41351e754b333ae627945ec649fd037285578f
progresso85 <martin.heuripeau@gmail.com>
2021-10-14 16:18:02 +0200

    add question search in LeftPart
    add question search in LeftPart

--- my-app/src/components/LeftPart.vue
+++ my-app/src/components/LeftPart.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
       <KnowledgeGraph />
+      <Question />
   </div>
 </template>
 
 <script>
+import Question from '@/components/Question.vue';
 import KnowledgeGraph from '@/components/KnowledgeGraph.vue';
 export default {
     name: 'LeftPart',
     components: {
         KnowledgeGraph,
+        Question,
     }
 }
 </script>
---
+++ my-app/src/components/Question.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h3 class="card-title">
+                <span>Autres questions posées</span>
+            </h3>
+            <div class="card-q">
+                    <div class="card-r">Question 1 <br>
+                        <a href="#">Recherche</a>
+                    </div>
+
+                    <hr WIDTH="240">
+
+                    <div class="card-r">Question 2 <br>
+                        <a href="#">Recherche</a>
+                    </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Question',
+}
+</script>
+
+<style>
+.card{
+    max-width: 320px;
+    margin-left: 1em;
+    text-align: start;
+}
+
+.hr{
+    border: none;
+    border-top: 3px double #333;
+    color: #333;
+    overflow: visible;
+    text-align: center;
+    height: 5px;
+}
+
+hr:after {
+    background: #fff;
+    padding: 0 4px;
+    position: relative;
+    top: -13px;
+}   
+.card-r{
+    padding-left: 5px;
+}
+.card{
+    margin-bottom: 1rem;
+}
+</style>
