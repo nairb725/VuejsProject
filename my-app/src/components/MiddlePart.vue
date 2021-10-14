8e4bd907da1a3b2cc64aea0e856cf6e7b3777f78
Brian <nairb725@gmail.com>
2021-10-14 15:38:55 +0200

    composant AllListObject.vue

--- my-app/src/components/MiddlePart.vue
+++ my-app/src/components/MiddlePart.vue
@@ -1,61 +1,23 @@
 <template>
-  <div class="search">
-      <div>
-        <h1 class="title">{{title}}https://openclassrooms.com/forum/sujet/limiter-le-nombre-de-caractere-dans-un-div-width</h1>
-            <h2 class="display_url">{{display_url}}https://www.linguee.fr/anglais-francais/traduction/programming.html</h2>
-          
-            <h3 class="url">
-                <input class="searchbar" type="text" value="http://localhost:8083/Result">
-                <button class="btn btn-primary btn-block" v-on:click="search">copy link</button>
-            </h3>
-      
-            <hr  WIDTH="841">
-        </div>
+  <div>
+      <AllListObject/>
+      <AllListObject/>
+      <AllListObject/>
   </div>
 
 
 </template>
 
 <script>
+import AllListObject from '@/components/AllListObject.vue';
 export default {
-
+    name: 'MiddlePart',
+    components: {
+        AllListObject,
+    }
 }
 </script>
 
-<style>
-.title{
-    font-size: 40px;
-    margin-left: 50px;
-    word-wrap: break-word;
-    overflow: hidden;
-    white-space: nowrap;
-    
-}
-.display_url{
-    font-size: 15px;
-    margin-left: 50px;
-}
-.url{
-    font-size: 20px;
-    margin-left: 50px;
-}
-.search{
-    text-align: left;
-}
-.btn-primary{
-    border-radius: 0 10px 10px 0;
-}
-.searchbar{
-width: 85%;
-border-radius: 10px 0 0 10px;
-border-color: #d4d4d4;
-border-style:solid;
-border-right: none;
-padding-top: 4px;
-padding-bottom: 7px;
-padding-left:10px;
-background-color: #eeeeee;
-color: black;
-font-style:italic;
-}
+<style scoped>
+
 </style>
